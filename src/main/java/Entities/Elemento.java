fcfbb297f49e0bde9eb840aa326c7a4411752b85
SERENA FERRARO <serenaferraro21@gmail.com>
2025-01-24 12:33:01 +0100

    Set up

---
+++ src/main/java/Entities/Elemento.java
@@ -0,0 +1,93 @@
+package Entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)  // Strategia di ereditarietà
+@Table(name = "elementi")
+public abstract class Elemento {  // Aggiungi 'abstract' qui
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String codiceISBN;
+
+    @Column(nullable = false)
+    private String titolo;
+
+    @Column(name = "anno_pubblicazione", nullable = false)
+    private int annoPubblicazione;
+
+    @Column(name = "numero_pagine", nullable = false)
+    private int numeroPagine;
+
+    // Costruttore vuoto (richiesto da JPA)
+    public Elemento() {
+    }
+
+    // Costruttore
+    public Elemento(String codiceISBN, String titolo, int annoPubblicazione, int numeroPagine) {
+        this.codiceISBN = codiceISBN;
+        this.titolo = titolo;
+        this.annoPubblicazione = annoPubblicazione;
+        this.numeroPagine = numeroPagine;
+    }
+
+    // Getter e Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodiceISBN() {
+        return codiceISBN;
+    }
+
+    public void setCodiceISBN(String codiceISBN) {
+        this.codiceISBN = codiceISBN;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public int getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public void setAnnoPubblicazione(int annoPubblicazione) {
+        this.annoPubblicazione = annoPubblicazione;
+    }
+
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
+
+    public void setNumeroPagine(int numeroPagine) {
+        this.numeroPagine = numeroPagine;
+    }
+
+    // Metodo toString (utile per debug)
+    @Override
+    public String toString() {
+        return "Elemento{" +
+                "id=" + id +
+                ", codiceISBN='" + codiceISBN + '\'' +
+                ", titolo='" + titolo + '\'' +
+                ", annoPubblicazione=" + annoPubblicazione +
+                ", numeroPagine=" + numeroPagine +
+                '}';
+    }
+
+
+    }
